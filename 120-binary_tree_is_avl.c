c556f1af9304cc73fdae489c3c0c21d1dd6dd349
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-03-29 22:38:43 +0000

    Create file with filler code for the task

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_avl - Checks if a given binary tree is an AVL tree
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is an AVL tree, 0 otherwise
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (tree)
+		return (1);
+	return (0);
+}
